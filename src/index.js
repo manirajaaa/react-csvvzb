4fdcc931cbc84ce4ef15aabfbc7a47dc13282562
manirajaaa <manirajanakka123@gmail.com>
2022-03-08 18:29:58 +0530

    2 files modified, 2 added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 //import AppAccessingAvariable from "./AppAccessingAvariable"
 //import AppAttributesExp from "./AppAttributesExp";
 //import CreatCompAlgExp from "./CreatCompAlgeExp";
-import ConditionalRendering from "./ConditionalRendering"
+//import ConditionalRendering from "./ConditionalRendering"
 
 
 //ReactDOM.render(<App />, document.getElementById("root"));
@@ -14,5 +14,12 @@ import ConditionalRendering from "./ConditionalRendering"
 //ReactDOM.render(<AppAccessingAvariable />,document.getElementById("root"));
 //ReactDOM.render(<AppAttributesExp />,document.getElementById("root"));
 //ReactDOM.render(<CreatCompAlgExp />,document.getElementById("root"));
-ReactDOM.render(<ConditionalRendering />,document.getElementById("root"));
+//ReactDOM.render(<ConditionalRendering />,document.getElementById("root"));
+
+
+//Styling
+import './App.css';
+import AppComp from './AppComp';
+ReactDOM.render(<AppComp />, document.getElementById('root'));
+
 
