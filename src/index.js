af151901967e6f987c1db7916bbbe77aced4df08
manirajaaa <manirajanakka123@gmail.com>
2022-03-08 19:46:38 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //import App from "./App";
+//ReactDOM.render(<App />, document.getElementById("root"));
+
 //import AppBasicsV1 from "./AppBasicsV1";
+//ReactDOM.render(<AppBasicsV1 />,document.getElementById("root"));
+
 //import AppAccessingAvariable from "./AppAccessingAvariable"
+//ReactDOM.render(<AppAccessingAvariable />,document.getElementById("root"));
+
 //import AppAttributesExp from "./AppAttributesExp";
+//ReactDOM.render(<AppAttributesExp />,document.getElementById("root"));
+
 //import CreatCompAlgExp from "./CreatCompAlgeExp";
+//ReactDOM.render(<CreatCompAlgExp />,document.getElementById("root"));
+
 //import ConditionalRendering from "./ConditionalRendering"
+//ReactDOM.render(<ConditionalRendering />,document.getElementById("root"));
 
 //Image applications
 //import PngApp from "./PngApp";
 //ReactDOM.render(<PngApp/>,document.getElementById('root'));
-
-//ReactDOM.render(<App />, document.getElementById("root"));
-//ReactDOM.render(<AppBasicsV1 />,document.getElementById("root"));
-//ReactDOM.render(<AppAccessingAvariable />,document.getElementById("root"));
-//ReactDOM.render(<AppAttributesExp />,document.getElementById("root"));
-//ReactDOM.render(<CreatCompAlgExp />,document.getElementById("root"));
-//ReactDOM.render(<ConditionalRendering />,document.getElementById("root"));
 
 
 //Styling
